1178e6b433f756bd1aec4641fbadcf792f3b4989
siman-man <k128585@ie.u-ryukyu.ac.jp>
2015-08-17 01:11:09 +0900

    8120.462282262851

--- PopulationMapping.cpp
+++ PopulationMapping.cpp
@@ -42,7 +42,7 @@ enum MODE {
 typedef struct Coord {
   int y;
   int x;
-  
+
   Coord(int y = UNDEFINED, int x = UNDEFINED){
     this->y = y;
     this->x = x;
@@ -126,9 +126,26 @@ typedef struct Area {
   }
 } AREA;
 
+// 地域リスト
+vector<AREA> g_areaList;
+
 // 世界地図
 CELL g_worldMap[MAX_H][MAX_W];
 
+typedef struct Node {
+  int population;
+  int landCount;
+  vector<int> areaIdList;
+
+  Node(int population = 0, int landCount = 0){
+    this->population = population;
+    this->landCount = landCount;
+  }
+
+  bool operator >(const Node &e) const{
+    return landCount < e.landCount;
+  }    
+} NODE;
 
 class PopulationMapping {
   public:
@@ -204,7 +221,6 @@ class PopulationMapping {
 
     vector<AREA> research(){
       vector<AREA> result;
-      vector<AREA> areaList;
       priority_queue<AREA, vector<AREA>, greater<AREA> > que;
       priority_queue<AREA, vector<AREA>, greater<AREA> > fque;
       AREA rootArea(g_height-1, 0, 0, g_width-1);
@@ -215,6 +231,7 @@ class PopulationMapping {
 
       for(int i = 0; i < divideCount && !que.empty(); i++){
         AREA area = que.top(); que.pop();
+        vector<AREA> areaList;
         int tempPopulation = area.population;
         int sumPopulation = 0;
         //fprintf(stderr,"areaId = %d, population = %d\n", area.id, area.population);
@@ -261,172 +278,217 @@ class PopulationMapping {
       int currentPopulation = g_totalPopulation;
       g_mode = FINAL;
 
+
       while(!que.empty()){
         AREA area = que.top(); que.pop();
         fque.push(area);
       }
 
+      fprintf(stderr,"fque size = %ld\n", fque.size());
+
       while(!fque.empty() && currentPopulation >= g_limitPopulation){
         AREA area = fque.top(); fque.pop();
+        g_areaList.push_back(area);
+
+        if(currentPopulation - area.population < g_limitPopulation){
+          fque.push(area);
+          break;
+        }
         currentPopulation -= area.population;
       }
 
       fprintf(stderr,"currentPopulation = %d\n", currentPopulation);
+      vector<AREA> selectedArea;
 
       while(!fque.empty()){
         AREA area = fque.top(); fque.pop();
-        selectArea(area.y1, area.x1, area.y2, area.x2);
+        selectedArea.push_back(area);
+        g_areaList.push_back(area);
+        //selectArea(area.y1, area.x1, area.y2, area.x2);
       }
+
+      int sumPopulation = 0;
+      int sumLandCount = 0;
+
+      for(int i = 0; i < selectedArea.size(); i++){
+        AREA area = selectedArea[i];
+        sumPopulation += area.population;
+        sumLandCount += area.landCount;
+      }
+
+      int bestId;
+      int maxLandCount = INT_MIN;
+
+      for(int i = 0; i < selectedArea.size(); i++){
+        AREA area = selectedArea[i];
+        int p = sumPopulation - area.population;
+        int lc = sumLandCount - area.landCount;
+
+        if(p < g_limitPopulation && maxLandCount < lc){
+          maxLandCount = lc;
+          bestId = area.id;
+        }
+      }
+
+      for(int i = 0; i < selectedArea.size(); i++){
+        AREA area = selectedArea[i];
+
+        if(bestId != area.id){
+          selectArea(area.y1, area.x1, area.y2, area.x2);
+        }
+      }
+
+      fprintf(stderr,"g_areaList size = %ld\n", g_areaList.size());
 
       return result;
     }
 
-    /*
-     *   p1  p2  p3
-     *    +--+--+
-     *    |  |  |
-     * p4 +--+--+ p6
-     *    |  |  |
-     *    +--+--+
-     *  p7  p8  p9
-     */
-    vector<AREA> divideArea2(AREA area){
-      int height = abs(area.y1-area.y2) + 1;
-      int width  = abs(area.x2-area.x1) + 1;
-      //fprintf(stderr,"height = %d, width = %d\n", height, width);
-      int diff_h = height/2-1;
-      int diff_w = width/2-1;
+      /*
+       *   p1  p2  p3
+       *    +--+--+
+       *    |  |  |
+       * p4 +--+--+ p6
+       *    |  |  |
+       *    +--+--+
+       *  p7  p8  p9
+       */
+      vector<AREA> divideArea2(AREA area){
+        int height = abs(area.y1-area.y2) + 1;
+        int width  = abs(area.x2-area.x1) + 1;
+        //fprintf(stderr,"height = %d, width = %d\n", height, width);
+        int diff_h = height/2-1;
+        int diff_w = width/2-1;
 
-      COORD p1(area.y2,        area.x1);
-      COORD p2(area.y2,        area.x1+diff_w);
-      COORD p3(area.y2,        area.x2);
-      COORD p4(area.y2+diff_h, area.x1);
-      COORD p5(area.y2+diff_h, area.x1+diff_w);
-      COORD p6(area.y2+diff_h, area.x2);
-      COORD p7(area.y1,        area.x1);
-      COORD p8(area.y1,        area.x1+diff_w);
-      COORD p9(area.y1,        area.x2);
+        COORD p1(area.y2,        area.x1);
+        COORD p2(area.y2,        area.x1+diff_w);
+        COORD p3(area.y2,        area.x2);
+        COORD p4(area.y2+diff_h, area.x1);
+        COORD p5(area.y2+diff_h, area.x1+diff_w);
+        COORD p6(area.y2+diff_h, area.x2);
+        COORD p7(area.y1,        area.x1);
+        COORD p8(area.y1,        area.x1+diff_w);
+        COORD p9(area.y1,        area.x2);
 
-      AREA area1;
-      AREA area2;
+        AREA area1;
+        AREA area2;
 
-      if(height > width){
-        area1 = AREA(p4.y-1, p4.x, p3.y, p3.x);
-        area2 = AREA(p7.y, p7.x, p6.y, p6.x);
-      }else{
-        area1 = AREA(p7.y, p7.x, p2.y, p2.x);
-        area2 = AREA(p8.y, p8.x+1, p3.y, p3.x);
-      }
-
-      area1.dividedCount = area.dividedCount + 1;
-      area2.dividedCount = area.dividedCount + 1;
-
-      vector<AREA> areaList;
-      areaList.push_back(area1);
-      areaList.push_back(area2);
-
-      return areaList;
-    }
-
-    /*
-     *   p1  p2  p3
-     *    +--+--+
-     *    |  |  |
-     * p4 +--+--+ p6
-     *    |  |  |
-     *    +--+--+
-     *  p7  p8  p9
-     */
-    vector<AREA> divideArea4(AREA area){
-      int height = area.y1 - area.y2 + 1;
-      int width  = area.x2 - area.x1 + 1;
-      //fprintf(stderr,"height = %d, width = %d\n", height, width);
-      int diff_h = height/2-1;
-      int diff_w = width/2-1;
-
-      COORD p1(area.y2,        area.x1);
-      COORD p2(area.y2,        area.x1+diff_w);
-      COORD p3(area.y2,        area.x2);
-      COORD p4(area.y2+diff_h, area.x1);
-      COORD p5(area.y2+diff_h, area.x1+diff_w);
-      COORD p6(area.y2+diff_h, area.x2);
-      COORD p7(area.y1,        area.x1);
-      COORD p8(area.y1,        area.x1+diff_w);
-      COORD p9(area.y1,        area.x2);
-
-      AREA area1 = AREA(p4.y, p4.x, p2.y, p2.x);
-      AREA area2 = AREA(p5.y, p5.x+1, p3.y, p3.x);
-      AREA area3 = AREA(p7.y, p7.x, p5.y+1, p5.x);
-      AREA area4 = AREA(p8.y, p8.x+1, p6.y+1, p6.x);
-
-      area1.dividedCount = area.dividedCount + 1;
-      area2.dividedCount = area.dividedCount + 1;
-      area3.dividedCount = area.dividedCount + 1;
-      area4.dividedCount = area.dividedCount + 1;
-
-      vector<AREA> areaList;
-      areaList.push_back(area1);
-      areaList.push_back(area2);
-      areaList.push_back(area3);
-      areaList.push_back(area4);
-
-      return areaList;
-    }
-
-    vector <string> mapPopulation(int maxPercentage, vector<string> worldMap, int totalPopulation){
-      init(maxPercentage, worldMap, totalPopulation);
-      vector<string> result;
-      research();
-
-      for(int y = 0; y < g_height; y++){
-        string line = "";
-
-        for(int x = 0; x < g_width; x++){
-          CELL *cell = getCell(y, x);
-
-          if(cell->selected){
-            line += 'X';
-          }else{
-            line += '.';
-          }
+        if(height > width){
+          area1 = AREA(p4.y-1, p4.x, p3.y, p3.x);
+          area2 = AREA(p7.y, p7.x, p6.y, p6.x);
+        }else{
+          area1 = AREA(p7.y, p7.x, p2.y, p2.x);
+          area2 = AREA(p8.y, p8.x+1, p3.y, p3.x);
         }
-        result.push_back(line);
+
+        area1.dividedCount = area.dividedCount + 1;
+        area2.dividedCount = area.dividedCount + 1;
+
+        vector<AREA> areaList;
+        areaList.push_back(area1);
+        areaList.push_back(area2);
+
+        return areaList;
       }
 
-      return result;
-    }
+      /*
+       *   p1  p2  p3
+       *    +--+--+
+       *    |  |  |
+       * p4 +--+--+ p6
+       *    |  |  |
+       *    +--+--+
+       *  p7  p8  p9
+       */
+      vector<AREA> divideArea4(AREA area){
+        int height = area.y1 - area.y2 + 1;
+        int width  = area.x2 - area.x1 + 1;
+        //fprintf(stderr,"height = %d, width = %d\n", height, width);
+        int diff_h = height/2-1;
+        int diff_w = width/2-1;
 
-    void showMap(){
-      for(int y = 0; y < g_height; y++){
-        for(int x = 0; x < g_width; x++){
-          CELL *cell = getCell(y,x);
+        COORD p1(area.y2,        area.x1);
+        COORD p2(area.y2,        area.x1+diff_w);
+        COORD p3(area.y2,        area.x2);
+        COORD p4(area.y2+diff_h, area.x1);
+        COORD p5(area.y2+diff_h, area.x1+diff_w);
+        COORD p6(area.y2+diff_h, area.x2);
+        COORD p7(area.y1,        area.x1);
+        COORD p8(area.y1,        area.x1+diff_w);
+        COORD p9(area.y1,        area.x2);
 
-          if(cell->isLand()){
-            fprintf(stderr,"X");
-          }else{
-            fprintf(stderr,".");
+        AREA area1 = AREA(p4.y, p4.x, p2.y, p2.x);
+        AREA area2 = AREA(p5.y, p5.x+1, p3.y, p3.x);
+        AREA area3 = AREA(p7.y, p7.x, p5.y+1, p5.x);
+        AREA area4 = AREA(p8.y, p8.x+1, p6.y+1, p6.x);
+
+        area1.dividedCount = area.dividedCount + 1;
+        area2.dividedCount = area.dividedCount + 1;
+        area3.dividedCount = area.dividedCount + 1;
+        area4.dividedCount = area.dividedCount + 1;
+
+        vector<AREA> areaList;
+        areaList.push_back(area1);
+        areaList.push_back(area2);
+        areaList.push_back(area3);
+        areaList.push_back(area4);
+
+        return areaList;
+      }
+
+      vector <string> mapPopulation(int maxPercentage, vector<string> worldMap, int totalPopulation){
+        init(maxPercentage, worldMap, totalPopulation);
+        vector<string> result;
+        research();
+
+        for(int y = 0; y < g_height; y++){
+          string line = "";
+
+          for(int x = 0; x < g_width; x++){
+            CELL *cell = getCell(y, x);
+
+            if(cell->selected){
+              line += 'X';
+            }else{
+              line += '.';
+            }
           }
+          result.push_back(line);
         }
-        fprintf(stderr,"\n");
-      }
-    }
-};
 
-int main(){
-  int maxPercentage,totalPopulation,height;string line;vector<string> worldMap;
-  cin >> maxPercentage;
-  cin >> height;
-  
-  for(int i = 0; i < height; i++){
-    cin >> line;
-    worldMap.push_back(line);
-  }
-  scanf("%d", &totalPopulation);
-  PopulationMapping pm;
-  vector<string> ret = pm.mapPopulation(maxPercentage, worldMap, totalPopulation);
-  cout << ret.size() << endl;
-  for(int i = 0; i < ret.size(); i++){
-    cout << ret[i] << endl;
-  }
-  return 0;
-}
+        return result;
+      }
+
+      void showMap(){
+        for(int y = 0; y < g_height; y++){
+          for(int x = 0; x < g_width; x++){
+            CELL *cell = getCell(y,x);
+
+            if(cell->isLand()){
+              fprintf(stderr,"X");
+            }else{
+              fprintf(stderr,".");
+            }
+          }
+          fprintf(stderr,"\n");
+        }
+      }
+    };
+
+    int main(){
+      int maxPercentage,totalPopulation,height;string line;vector<string> worldMap;
+      cin >> maxPercentage;
+      cin >> height;
+
+      for(int i = 0; i < height; i++){
+        cin >> line;
+        worldMap.push_back(line);
+      }
+      scanf("%d", &totalPopulation);
+      PopulationMapping pm;
+      vector<string> ret = pm.mapPopulation(maxPercentage, worldMap, totalPopulation);
+      cout << ret.size() << endl;
+      for(int i = 0; i < ret.size(); i++){
+        cout << ret[i] << endl;
+      }
+      return 0;
+    }
